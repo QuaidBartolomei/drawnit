c3cfb9310f12e440ed75e28753b751a71dd01e76
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2021-01-01 14:43:07 -0800

    added serve-favicon

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,7 @@ import { initSocketServer } from 'loaders/socket.loader';
 import env from 'utils/env.utils';
 import express from 'express';
 import path from 'path';
+import favicon from 'serve-favicon';
 
 initDb(env.MONGO_DB_URI || '');
 const app = express();
@@ -12,9 +13,15 @@ const server = initApp(app, () => {
 });
 initSocketServer(server);
 
-app.use('/static', express.static(path.join(__dirname, '../../client/build/static')));
-app.get('*', (req, res) => {
-  res.sendFile('index.html', {
-    root: path.join(__dirname, '../../client/build/'),
+const client = path.join(__dirname, '../../client/build');
+const staticPath = path.join(client, 'static');
+const faviconPath = path.join(client, 'favicon.ico');
+
+app
+  .use('/static', express.static(staticPath))
+  .use(favicon(faviconPath))
+  .get('*', (req, res) => {
+    res.sendFile('index.html', {
+      root: client,
+    });
   });
-});
