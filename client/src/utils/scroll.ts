318050f63ee8b3929855fd9cc4160d962b78eccd
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2021-11-23 23:20:39 -0800

    fix: migrate room page

---
+++ client/src/utils/scroll.ts
@@ -0,0 +1,9 @@
+export function scrollToMiddle(scrollingElementId: string) {
+  const scrollingElement = document.getElementById(scrollingElementId);
+  if (!scrollingElement)
+    return console.error(`element with id: "${scrollingElementId}" not found`);
+  const viewportWidth = window.innerWidth;
+  const fullWidth = scrollingElement.scrollWidth;
+  const midpoint = (fullWidth - viewportWidth) / 2;
+  scrollingElement.scrollTo(midpoint, 0);
+}
