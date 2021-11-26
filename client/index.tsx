7f0e91684bb4c69e8fde378b5e3ae2faaccdbcd6
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2021-11-26 13:37:14 -0800

    fix: pages

--- client/index.tsx
+++ client/index.tsx
@@ -1,31 +1,28 @@
 // import * as serviceWorker from './serviceWorker';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import 'fontsource-roboto';
-import Homepage from 'pages/Home/Home.page';
-import RoomPage from 'pages/Room/Room.page';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { PageRoutes } from 'routes/page.routes';
-import 'global-styles.css';
-const routes = PageRoutes();
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Homepage from 'pages/Home'
+import RoomPage from 'pages/Room.page'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { PageRoutes } from 'routes'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
 
 function App() {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient()
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <CssBaseline />
         <Switch>
-          <Route exact path={routes.HOME} component={Homepage} />
-          <Route path={routes.ROOM} component={RoomPage} />
+          <Route exact path={PageRoutes.HOME} component={Homepage} />
+          <Route path={PageRoutes.ROOM} component={RoomPage} />
         </Switch>
       </BrowserRouter>
     </QueryClientProvider>
-  );
+  )
 }
 
 // If you want your app to work offline and load faster, you can change
