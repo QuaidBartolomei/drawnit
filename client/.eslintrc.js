8528d2be5532e3dafc2beb367a02c42dc5eaf956
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2021-11-26 02:58:37 -0800

    chore: client eslint settings

---
+++ client/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: [
+    'next/core-web-vitals',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: './',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  rules: {},
+}
