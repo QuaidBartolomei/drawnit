66b156416895e15084899f660274de8e07b90cff
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2021-12-04 01:11:26 -0800

    feat: client import linting

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -5,6 +5,8 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
@@ -22,10 +24,20 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'import/no-unresolved': 'error',
   },
   settings: {
     react: {
       version: 'detect', // React version. "detect" automatically picks the version you have installed.
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+        project: './',
+      },
     },
   },
 }
