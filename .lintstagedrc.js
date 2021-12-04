74ec7b5067805f11314e0604b8e8e65ae71765e0
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2021-12-04 01:14:33 -0800

    docs: commented lintstaged file

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,15 @@
 module.exports = {
-  '**/package.json': 'npx sort-package-json',
+  // sort package.json if changed
+  '**/package.json': 'sort-package-json',
+
+  // format all file types recognized by prettier
   '*': 'prettier --ignore-unknown --write',
+
+  // lint typescript files
   'client/**/*.{ts,tsx}': 'npm run client:lint-staged --',
   'server/**/*.{ts,tsx}': 'npm run server:lint-staged --',
+
+  // lint entire project if eslint settings changed
   'client/**/.eslint*': 'npm run client:lint',
   'server/**/.eslint*': 'npm run server:lint',
 }
