a1ae14cfc9343b73ecfa280df473a353b68fdabc
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2021-12-05 23:34:33 -0800

    fix: remove jest-mongo

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,8 +1,6 @@
 const tsPreset = require('ts-jest/jest-preset')
-const mongodbPreset = require('@shelf/jest-mongodb/jest-preset')
 module.exports = {
   ...tsPreset,
-  ...mongodbPreset,
   coveragePathIgnorePatterns: ['/node_modules/'],
   modulePaths: ['<rootDir>/src'],
   globalSetup: '<rootDir>/test/globalSetup.ts',
