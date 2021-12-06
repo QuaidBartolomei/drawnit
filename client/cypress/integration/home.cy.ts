914383102c5ff0e0e9549783f77b48ce9e8076ea
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2021-12-05 22:37:37 -0800

    test: add screenshot

--- client/cypress/integration/home.cy.ts
+++ client/cypress/integration/home.cy.ts
@@ -1,4 +1,9 @@
-it('homepage should create room and redirect', () => {
+before(() => {
   cy.visit('http://localhost:3000')
+})
+
+it('homepage should create room and redirect', () => {
   cy.url().should('contain', '/room')
+  cy.get('canvas')
+  cy.screenshot('screenshot')
 })
