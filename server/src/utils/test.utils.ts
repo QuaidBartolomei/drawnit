9425c2921253c4e422fbbaa270e41d0196037043
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2021-12-07 20:39:11 -0800

    chore: remove unused getAuthToken function

--- server/src/utils/test.utils.ts
+++ server/src/utils/test.utils.ts
@@ -1,6 +1,4 @@
-import axios from 'axios'
 import { readFileSync } from 'fs'
-import env from 'utils/env.utils'
 
 export const imageFile: {
   encodedFile: string
@@ -8,16 +6,4 @@ export const imageFile: {
 } = {
   encodedFile: readFileSync(__dirname + '/image.jpg', 'base64'),
   mimeType: '/image/jpeg',
-}
-
-export async function getAuth0Token() {
-  const url = `https://${env.AUTH0_DOMAIN}/oauth/token`
-  const data = {
-    client_id: env.AUTH0_CLIENT_ID,
-    client_secret: env.AUTH0_CLIENT_SECRET,
-    audience: env.AUTH0_AUDIENCE,
-    grant_type: 'client_credentials',
-  }
-  const res = await axios.post(url, data)
-  return `${res.data.token_type} ${res.data.access_token}`
 }
