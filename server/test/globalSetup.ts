fd2f84c87adbacdaa2379e9fce270e77bdf5d225
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2021-12-07 19:33:55 -0800

    chore: remove explicit any from jest global setups

--- server/test/globalSetup.ts
+++ server/test/globalSetup.ts
@@ -10,7 +10,8 @@ export default async function globalSetup() {
     // it's needed in global space, because we don't want to create a new instance every test-suite
     const instance = await MongoMemoryServer.create()
     const uri = instance.getUri()
-    ;(global as any).__MONGOINSTANCE = instance
+    ;(global as { __MONGOINSTANCE?: MongoMemoryServer }).__MONGOINSTANCE =
+      instance
     process.env.MONGO_URI = uri.slice(0, uri.lastIndexOf('/'))
   } else {
     process.env.MONGO_URI = `mongodb://${config.IP}:${config.Port}`
--- server/test/globalTeardown.ts
+++ server/test/globalTeardown.ts
@@ -4,7 +4,10 @@ import config from './config'
 export = async function globalTeardown() {
   if (config.Memory) {
     // Config to decided if an mongodb-memory-server instance should be used
-    const instance: MongoMemoryServer = (global as any).__MONGOINSTANCE
+    const instance = (global as { __MONGOINSTANCE?: MongoMemoryServer })
+      .__MONGOINSTANCE
+    if (!instance)
+      return console.error('global mongo memory server instance not found')
     await instance.stop()
   }
 }
