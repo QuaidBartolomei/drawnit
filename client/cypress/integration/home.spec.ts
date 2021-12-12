c13e04a66ec2d641ebbbbd3b4f05d2f5a8230caf
Quaid Bartolomei <quaid.bartolomei@gmail.com>
2021-12-12 02:28:18 -0800

    fix: cypress e2e test

--- client/cypress/integration/home.spec.ts
+++ client/cypress/integration/home.spec.ts
@@ -5,6 +5,7 @@ before(() => {
 it('homepage should create room and redirect', () => {
   cy.url().should('contain', '/room')
   cy.get('canvas')
+    .should('exist')
     .trigger('mousedown', { offsetX: 500, offsetY: 400, which: 1 })
     .trigger('mousemove', { offsetX: 500, offsetY: 500 })
     .trigger('mousemove', { offsetX: 510, offsetY: 500 })
